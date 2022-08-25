fd248f5cf96258302ad481db695ab3d425cbe69a
vietanhrmit <nva6880@gmail.com>
2022-08-25 22:23:06 +0700

    basic php

---
+++ php9/readme.txt
@@ -0,0 +1,2 @@
+cd into the 'www' folder and run the php server
+on your browser: go to localhost:{port}/homepage.php
---
+++ php9/www/homepage.php
@@ -0,0 +1,4 @@
+<a href="login.php"> Log in </a><br>
+<a href="signup_customer.php"> Sign up for customers</a><br>
+<a href="signup_vendor.php"> Sign up for vendors</a><br>
+<a href="signup_shipper.php"> Sign up for shippers</a><br>
