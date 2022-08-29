afb7a1d24ee187f07dfe795c0e3a197234e05067
vietanhrmit <nva6880@gmail.com>
2022-08-29 22:01:02 +0700

    log-in pfp working as intended

--- php9/www/welcome.php
+++ php9/www/welcome.php
@@ -7,6 +7,17 @@
     {
         printf("Hello %s %s, username %s. At the server, currently it is %s.<br>", $_SESSION['user']['type'],
         $_SESSION['user']['fullname'],$_SESSION['user']["uname"], date("g:i a"));
+        $pfp_dir = "pfp/";
+        $scan = scandir($pfp_dir);
+        foreach($scan as $file) {
+            if ($file == "." || $file == "..") { }//do nothing - just to be safe
+            else{
+                $file_uname = explode(".", $file)[0]; //username portion of img file
+                if($_SESSION['user']['uname'] == $file_uname){
+                    $_SESSION['user']['pfp'] = $pfp_dir . $file;
+                }
+            }
+         }
         if(isset($_SESSION['user']['pfp'])){
             echo "<img src=\"" . $_SESSION['user']['pfp'] . "\" width=\"200\">";
         }
